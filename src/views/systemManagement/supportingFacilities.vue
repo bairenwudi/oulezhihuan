e8bfcea6fd4e098f0626b3c87239005450e0d58b
ss1354295294 <ss1354295294>
2018-11-15 18:39:13 +0800

    配套设施

--- src/views/systemManagement/supportingFacilities.vue
+++ src/views/systemManagement/supportingFacilities.vue
@@ -10,21 +10,30 @@
               <Button type="primary" @click="addClick">新增</Button>
             </FormItem>
 
-            <FormItem>
+            <!-- <FormItem>
               <Button type="primary" @click="delClick">删除</Button>
-            </FormItem>
+            </FormItem> -->
         </Form>
 
-        <TableM :columns="columns" :data="userData" :loading="loading" :current.async="currentPageIndex" :total="total" @pageChange="pageChange"></TableM>
+        <TableM 
+          :columns="columns"
+          :data="userData"
+          :loading="loading"
+          :current.async="currentPageIndex"
+          :total="total"
+          @selectChange="watchSelectChange"
+          @pageChange="pageChange"
+        ></TableM>
         
         <!-- 新增提示框 -->
         <Modal v-model="addModal"
                 title="新增"
                 :mask-closable="false"
-                @on-ok="ModalConfirm('addForm')"
-                @on-cancel="ModalReset('addForm')"
+                @on-ok="AddModalConfirm('addForm')"
+                @on-cancel="AddModalReset('addForm')"
             >
             <Form ref="addForm" :model="addForm" :rules="addRules" :label-width="80">
+                
                 <FormItem label="设施名称" prop="facilities_name">
                     <Input v-model="addForm.facilities_name" placeholder="请输入设施名称"></Input>
                 </FormItem>
@@ -35,6 +44,7 @@
                         name="upLoad"
                         :action="actionUrl"
                         :data="addData"
+                        :file-list="fileList"
                         list-type="picture-card"
                         :auto-upload="false"
                         :on-change="onChange"
@@ -55,35 +65,54 @@
             </Form>
 
             <div slot="footer" align="center">
-                <Button type="primary" @click="ModalConfirm('addForm')" :loading="loading">确定</Button>
-                <Button @click="ModalReset('addForm')" style="margin-left: 8px">重置</Button>
+                <Button type="primary" @click="AddModalConfirm('addForm')" :loading="loading">确定</Button>
+                <Button @click="AddModalReset('addForm')" style="margin-left: 8px">取消</Button>
             </div>
         </Modal>
 
      <!--  编辑提示框 -->
-        <!-- <Modal v-model="editModal"
+        <Modal v-model="editModal"
                 title="编辑"
                 :mask-closable="false"
-                @on-ok="ModalConfirm('formValidate')"
-                @on-cancel="ModalReset('formValidate')"
+                @on-ok="EditModalConfirm('editForm')"
+                @on-cancel="EditModalReset('editForm')"
             >
-            <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
-                <FormItem label="设施名称" prop="facilities_name">
-                    <Input v-model="formValidate.facilities_name" placeholder="请输入banner名称"></Input>
+            <Form ref="editForm" :model="editForm" :rules="editRules" :label-width="80">
+                
+                 <FormItem label="设施名称" prop="facilities_name">
+                    <Input v-model="editForm.facilities_name" placeholder="请输入设施名称"></Input>
                 </FormItem>
+                
+                <FormItem label="设施图片" :label-width="85">
+                    <el-upload
+                        ref="editUpload"
+                        name="upLoad"
+                        :action="actionUrl"
+                        :data="editData"
+                        :file-list="fileList"
+                        list-type="picture-card"
+                        :auto-upload="false"
+                        :on-change="onChange"
+                        :on-preview="handlePictureCardPreview"
+                        :on-success="uploadSuccess"
+                        :on-error="uploadError"
+                        :on-exceed="uploadonExceed"
+                        :on-remove="handleRemove"
+                        :limit="1"
+                    >
+                        <i class="el-icon-plus"></i>
+                    </el-upload>
 
-                <FormItem label="设施图片" prop="upLoad" :label-width="85"> -->
-                    
-                    <!-- <Modal title="View Image" v-model="visible">
-                        <img :src="'https://o5wwk8baw.qnssl.com/' + imgName + '/large'" v-if="visible" style="width: 100%">
-                    </Modal> -->
-                <!-- </FormItem>
+                    <Modal :footer-hide="true" :transfer="false" title="预览图片" v-model="visible">
+                        <img :src="imgUrl" v-if="visible" style="width: 100%">
+                    </Modal>
+                </FormItem>
             </Form>
                 <div slot="footer" align="center">
-                    <Button type="primary" @click="ModalConfirm('formValidate')" :loading="loading">确定</Button>
-                    <Button @click="ModalReset('formValidate')" style="margin-left: 8px">重置</Button>
+                    <Button type="primary" @click="EditModalConfirm('editForm')" :loading="loading">确定</Button>
+                    <Button @click="EditModalReset('editForm')" style="margin-left: 8px">取消</Button>
                 </div>
-        </Modal> -->
+        </Modal>
 
     <!-- 删除提示框 -->
     <Modal v-model="delDilaog" width="360">
@@ -109,7 +138,7 @@ import {
   supportingFacilitiesAdd, //配套设施管理添加
   supportingFacilitiesEdit, //配套设施管理编辑
   supportingFacilitiesDel, //配套设施管理删除
-  getBase, // 获取域名
+  getBase, // 获取域名  
 } from "../../api/lp-systemManagement/api.js";
 
 export default {
@@ -128,17 +157,20 @@ export default {
     };
 
     return {
+      
 
-      fileList: [],
+      editFileList: [],  // 编辑默认图片列表
 
-      formValidate: {
-        // 定义新增表单的对象
-        facilities_name: "",
-        upLoad: ""
-      },
+      selection: [],   // 存放select选中的数组
 
       addForm: {
         facilities_name: ""
+      },
+
+      editForm: {
+        // 定义新增表单的对象
+        facilities_name: "",
+        upLoad: ""
       },
 
       addRules: {
@@ -148,7 +180,16 @@ export default {
         ]
       },
 
+      editRules: {
+        // 定义表单的校验规则
+        facilities_name: [
+          { required: true, message: "请输入设施名称", trigger: "blur" }
+        ]
+      },
+
       addData: {},
+
+      editData: {},
 
       addModal: false,
 
@@ -157,6 +198,8 @@ export default {
       delDilaog: false, // 控制删除弹出框
 
       delLoading: false, // 控制删除按钮loading
+
+      deletefacilities_id:"",//删除需要的facilities_id
 
       currentPageIndex: 1, // 当前页
 
@@ -199,7 +242,7 @@ export default {
                     height: "80px",
                     padding:"12px"
                   }
-                },
+                }
              );
           }
         },
@@ -226,7 +269,6 @@ export default {
                 {
                   props: {
                     type: "primary",
-                    size: "small"
                   },
                   style: {
                     marginRight: "5px"
@@ -238,13 +280,29 @@ export default {
                   }
                 },
                 "编辑"
-              )
+              ),
+              h(
+                "Button",
+                {
+                props: {
+                    type: "primary",
+                },
+                on: {
+                    click: () => {
+                        this.delClick(params);
+                    }
+                }
+                },
+                "删除"
+            )
             ]);
           }
         }
       ],
 
       userData: [], // 内容数据
+
+      delArr:[],//删除 发送数组
 
       total: 0, // 总页数
 
@@ -295,10 +353,20 @@ export default {
         console.log(file, fileList);
     },
 
+    // 清空默认图片列表
+    resetFileList(fileList) {
+      this[fileList] = [];
+    },
+
     // 重置页数
     resetTotal() {
       this.currentPage = 1;
       this.total = 1;
+    },
+
+    // 监听select多选框触发事件
+    watchSelectChange(selection) {
+      this.selection = selection;
     },
 
     // 执行新增的事件
@@ -308,58 +376,146 @@ export default {
       }  
       this.addModal = true;  
     },
-
-    // 点击确定按钮
-    ModalConfirm(name) {
-        this.$refs[name].validate(valid => {
-            if (valid) {
-                // if(!this.fileList.length) {
-                //   this.$Message.warning('请上传图片');
-                //   return
-                // }
-                this.addData = this[name];
-                setTimeout(() => {
-                  this.$refs.addUpload.submit();
-                  this.$Message.success("保存成功!");
-                  this.addModal = false;
-                  this.loading = false;
-                },400);
-            }
-        });
+    
+    // 点击新增确定按钮
+    AddModalConfirm(name) {
+      this.$refs[name].validate(valid => {
+          if (valid) {
+            supportingFacilitiesAdd(this[name]).then(res =>{
+               if(!this.fileList.length) {
+                this.$Message.warning('请上传图片');
+                return
+              }
+              this.addData = this[name];
+              setTimeout(() => {
+                this.$refs.addUpload.submit();
+                if(res.data === 1){
+                    this.$Message.success("Success!");
+                    this.getUser();
+                  }else{
+                    this.$Message.error("Fail!");
+                  }
+                this.addModal = false;
+                this.loading = false;
+              }, 400);
+            })             
+          }
+      });
     },
 
-    // 点击框取消按钮
-    ModalReset(name) {
-      this.$refs[name].resetFields();
-      this.$Message.info("已取消");
+    // 点击新增框取消按钮
+    AddModalReset(name) {
+      this.addModal = false;
     },
 
     imgFun(val) {
-      // console.log(val);
-      // console.log(this.base); //http://192.168.1.39:8080
       return this.imgUrlFormat(val.facilities_pic_url, val.facilities_pic_name);
     },
 
     // 执行table编辑的事件
-    editClick(params) {
-      this.editModal = true;
-      console.log(params);
+    editClick({ row }) {
+      console.log(this.editFileList)
+      this.editForm = row;
+      this.editFileList.push(
+        { url: this.imgUrlFormat(row.facilities_pic_url, row.facilities_pic_name)}
+      );
+      this.$nextTick(() =>{
+        this.editModal = true;
+      })
+    },
+
+    // 点击编辑确定按钮
+    EditModalConfirm(formName) {
+      this.$refs[formName].validate((valid) =>{
+        if (valid) {
+          supportingFacilitiesEdit(this[formName]).then(res => {
+            console.log(res);
+            if(!this.fileList.length) {
+              this.$Message.warning('请上传图片');
+              return
+            }
+
+            this.editData = {
+              facilities_name: this[formName].facilities_name,
+            };
+
+            setTimeout(() => {
+              this.$refs.editUpload.submit();
+              this.$Message.success("保存成功!");
+              this.editModal = false;
+              this.loading = false;
+              this.resetFileList('editFileList');
+            },400);
+          })
+        }
+      })
+    },
+
+    // 点击编辑框取消按钮
+    EditModalReset(name) {
+      // this.$refs[name].resetFields();
+      // this.$Message.info("已取消");
+
+      this.editModal = false;
+      this.resetFileList('editFileList');
+      
     },
 
     // 执行删除的事件
+
+    //多删执行删除的事件
+    // delClick({ row }) {
+    //   console.log(this.selection)
+    //   for(var i = 0;i<this.selection.length;i++){
+    //     this.delArr.push(this.selection[i].facilities_id)
+    //   }
+    //   if(!this.selection.length) {
+    //     this.$Message.warning('请选择');
+    //     return
+    //   };
+    //   this.delDilaog = true;
+    // },
+
+    // 单删执行删除的事件
     delClick(params) {
-      console.log(params);
+      this.deletefacilities_id = params.row.facilities_id;
       this.delDilaog = true;
     },
 
     // 删除确定按钮
-    delConfrmClick() {
+
+    // 多删删除确定按钮
+    // async delConfrmClick() {
+    //   this.delLoading = true;
+    //   return
+    //   const { data } = await supportingFacilitiesDel({facilities_id: [...this.delArr]});
+    //   console.log(data);
+    //   if(data === 1){
+    //     this.$Message.success("删除成功!");
+    //     this.delDilaog = false;
+    //     this.getUser();
+    //   }else{
+    //     this.$Message.error("删除失败!");
+    //   }
+    //   this.delLoading = false;
+    // },
+
+    // 单删删除确定按钮
+    async delConfrmClick() {
       this.delLoading = true;
-      setTimeout(() => {
+      let params = {
+        facilities_id:this.deletefacilities_id
+      }
+      const { data } = await supportingFacilitiesDel(params);
+      console.log(data);
+      if(data === 1){
+        this.$Message.success("删除成功!");
         this.delDilaog = false;
-        this.$Message.success("成功");
-        console.log("我滚了");
-      }, 1000);
+        this.getUser();
+      }else{
+        this.$Message.error("删除失败!");
+      }
+      this.delLoading = false;
     },
 
     // 获取时间
@@ -394,11 +550,11 @@ export default {
     },
 
     imgUrlFormat(facilities_pic_url, facilities_pic_name) {
-      var afterUpload = facilities_pic_url.split("/");
-      var newArr = afterUpload.slice(afterUpload.indexOf("upload"));
-      var newImgUrl = newArr.join("/");
-      var showUrl = this.base + "/" + newImgUrl + "/" + facilities_pic_name;
-      return showUrl;
+      this.$nextTick(() => {
+        var afterUpload = facilities_pic_url.split("static/")[1];
+        var showUrl = `${this.base}/${afterUpload}/${facilities_pic_name}`;
+        return showUrl;
+      })
     },
 
     // 配套设施管理列表
@@ -423,7 +579,7 @@ export default {
   },
   mounted() {
     this.getUser();
-    this.base = getBase().base2;
+    this.base = getBase().base3;
     this.actionUrl = `${this.base}/Facilities_managementController/save`;
   }
 };
