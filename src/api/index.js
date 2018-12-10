2a04d7f64635fc4155d06a44499a9da8f84955f9
ss1354295294 <ss1354295294>
2018-12-10 17:09:00 +0800

    更新

--- src/api/index.js
+++ src/api/index.js
@@ -16,19 +16,19 @@ export const CorrectpathObj = {
   // },
 
   // // 开发环境(本地)
-  // Correctpath: {
-  //   base1: 'http://test.zkjiashijie.com:8080',
+  Correctpath: {
+    base1: 'http://test.zkjiashijie.com:8080',
 
-  //   base2: 'http://test.zkjiashijie.com:8080',
+    base2: 'http://test.zkjiashijie.com:8080',
     
-  //   base3: 'http://test.zkjiashijie.com:8080',
+    base3: 'http://test.zkjiashijie.com:8080',
     
-  //   base4: 'http://test.zkjiashijie.com:8080',
+    base4: 'http://test.zkjiashijie.com:8080',
     
-  //   base5: 'http://test.zkjiashijie.com:8080',
+    base5: 'http://test.zkjiashijie.com:8080',
     
-  //   base6: 'http://test.zkjiashijie.com:8080'
-  // },
+    base6: 'http://test.zkjiashijie.com:8080'
+  },
 
   // Correctpath: {
   //   base1:'http://192.168.1.11:8848',//刘天杨
@@ -45,18 +45,18 @@ export const CorrectpathObj = {
   // }  
 
  // 开发环境(本地-路由侠)
-Correctpath: {
-  base1:'http://xiangsikai.e1.luyouxia.net:27912',//刘天杨
+// Correctpath: {
+//   base1:'http://xiangsikai.e1.luyouxia.net:27912',//刘天杨
 
-  base2:'http://xiangsikai.e1.luyouxia.net:27912',//常猛
+//   base2:'http://xiangsikai.e1.luyouxia.net:27912',//常猛
 
-  base3:'http://xiangsikai.e1.luyouxia.net:27912',//杨胜男
+//   base3:'http://xiangsikai.e1.luyouxia.net:27912',//杨胜男
 
-  base4:'http://xiangsikai.e1.luyouxia.net:27912',//常猛
+//   base4:'http://xiangsikai.e1.luyouxia.net:27912',//常猛
 
-  base5:'http://xiangsikai.e1.luyouxia.net:27912',//刘卓
+//   base5:'http://xiangsikai.e1.luyouxia.net:27912',//刘卓
   
-  base6:'http://xiangsikai.e1.luyouxia.net:27912'//刘天杨
-}
+//   base6:'http://xiangsikai.e1.luyouxia.net:27912'//刘天杨
+// }
 
 };
