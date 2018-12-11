8a6c801930c956fdb69bc4e066c10a0359345946
ss1354295294 <ss1354295294>
2018-12-11 17:10:47 +0800

    更新及修改bug

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 
 export const CorrectpathObj = {
-    // 生产环境(测试)
+    // 生产环境(正式)
     // Correctpath: {
     //   base1: 'http://xyo.zkjiashijie.com:8080',
 
@@ -15,32 +15,36 @@ export const CorrectpathObj = {
     //   base6: 'http://xyo.zkjiashijie.com:8080'
     // },
 
-    // // 开发环境(本地)
+
+    // 开发环境(测试) 
+    Correctpath: {
+        base1: 'http://test.zkjiashijie.com:8080',
+
+        base2: 'http://test.zkjiashijie.com:8080',
+
+        base3: 'http://test.zkjiashijie.com:8080',
+
+        base4: 'http://test.zkjiashijie.com:8080',
+
+        base5: 'http://test.zkjiashijie.com:8080',
+
+        base6: 'http://test.zkjiashijie.com:8080'
+    }
+
+
+    // 开发环境(本地)
     // Correctpath: {
-    //     base1: 'http://test.zkjiashijie.com:8080',
+    //     base1: 'http://192.168.1.42:8080', // 刘天杨
 
-    //     base2: 'http://test.zkjiashijie.com:8080',
+    //     base2: 'http://192.168.1.42:8080', // 常猛
 
-    //     base3: 'http://test.zkjiashijie.com:8080',
+    //     base3: 'http://192.168.1.42:8080', // 杨胜男
 
-    //     base4: 'http://test.zkjiashijie.com:8080',
+    //     base4: 'http://192.168.1.42:8080', // 常猛
 
-    //     base5: 'http://test.zkjiashijie.com:8080',
+    //     base5: 'http://192.168.1.42:8080', // 刘卓
 
-    //     base6: 'http://test.zkjiashijie.com:8080'
+    //     base6: 'http://192.168.1.42:8080' // 刘天杨
     // }
 
-    Correctpath: {
-        base1: 'http://192.168.1.42:8080', // 刘天杨
-
-        base2: 'http://192.168.1.42:8080', // 常猛
-
-        base3: 'http://192.168.1.42:8080', // 杨胜男
-
-        base4: 'http://192.168.1.42:8080', // 常猛
-
-        base5: 'http://192.168.1.42:8080', // 刘卓
-
-        base6: 'http://192.168.1.42:8080' // 刘天杨
-    }
-};
+  };
